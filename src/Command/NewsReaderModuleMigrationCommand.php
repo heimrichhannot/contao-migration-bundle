0312c53e592e47febb90d730a626999938f1a7c1
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-04-20 18:34:14 +0200

    newsreader

--- src/Command/NewsReaderModuleMigrationCommand.php
+++ src/Command/NewsReaderModuleMigrationCommand.php
@@ -2,6 +2,12 @@
 
 namespace HeimrichHannot\MigrationBundle\Command;
 
+use Contao\System;
+use HeimrichHannot\FilterBundle\Model\FilterConfigElementModel;
+use HeimrichHannot\FilterBundle\Model\FilterConfigModel;
+use HeimrichHannot\Haste\Util\StringUtil;
+use HeimrichHannot\ReaderBundle\Model\ReaderConfigModel;
+use HeimrichHannot\UtilsBundle\Model\ModelUtil;
 use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
 use UAParser\Exception\FileNotFoundException;
@@ -12,6 +18,22 @@ class NewsReaderModuleMigrationCommand extends AbstractModuleMigrationCommand
      * @var array
      */
     protected static $types = ['newsreader'];
+
+    /**
+     * @var array
+     */
+    protected $readerConfig;
+
+    /**
+     * @var array
+     */
+    protected $filterConfig;
+
+    /**
+     * Current module
+     * @var array
+     */
+    protected $module;
 
     /**
      * {@inheritdoc}
@@ -28,10 +50,12 @@ class NewsReaderModuleMigrationCommand extends AbstractModuleMigrationCommand
      */
     protected function migrate(array $module): int
     {
-        $readerConfig = [
+        $this->module = $module;
+
+        $this->readerConfig = [
             'tstamp'                     => time(),
             'dateAdded'                  => time(),
-            'title'                      => $module['name'],
+            'title'                      => $this->module['name'],
             'dataContainer'              => 'tl_news',
             'manager'                    => 'default',
             'item'                       => 'default',
@@ -41,7 +65,7 @@ class NewsReaderModuleMigrationCommand extends AbstractModuleMigrationCommand
             'limitFormattedFields'       => 1,
             'formattedFields'            => serialize(['headline', 'teaser', 'singleSRC']),
             'publishedField'             => 'published',
-            'itemTemplate'               => $module['news_template'] ? 'templates/' . $module['news_template'] . '.html.twig' : '',
+            'itemTemplate'               => $this->module['news_template'] ? 'templates/' . $this->module['news_template'] . '.html.twig' : '',
             'headTags'                   => serialize(
                 [
                     ['service' => 'huh.head.tag.title', 'pattern' => '%headline%'],
@@ -53,39 +77,121 @@ class NewsReaderModuleMigrationCommand extends AbstractModuleMigrationCommand
             )
         ];
 
-        $sql = "INSERT INTO tl_reader_config (" . implode(',', array_keys($readerConfig)) . ") VALUES (" . implode(',', array_map(function ($value) {
+        $sql = "INSERT INTO tl_reader_config (" . implode(',', array_keys($this->readerConfig)) . ") VALUES (" . implode(',', array_map(function ($value) {
                 return ':' . $value;
-            }, array_keys($readerConfig))) . ")";
+            }, array_keys($this->readerConfig))) . ")";
 
-        if ($this->queryBuilder->getConnection()->executeUpdate($sql, $readerConfig)) {
-            $readerConfigId = $this->queryBuilder->getConnection()->lastInsertId();
-            $this->output->writeln('Migrated "' . $module['name'] . '" (Module ID:' . $module['id'] . ') into new reader config with ID: ' . $readerConfigId . '.');
+        if ($this->queryBuilder->getConnection()->executeUpdate($sql, $this->readerConfig)) {
+            $this->readerConfig['id'] = $this->queryBuilder->getConnection()->lastInsertId();
+            $this->output->writeln('Migrated "' . $this->module['name'] . '" (Module ID:' . $this->module['id'] . ') into new reader config with ID: ' . $this->readerConfig['id'] . '.');
 
-            $updateSql = "UPDATE tl_module SET readerConfig = :readerConfig, `type`=:type WHERE id = :id";
+            $this->updateModule();
 
-            if ($this->queryBuilder->getConnection()->executeUpdate($updateSql, ['readerConfig' => $readerConfigId, 'id' => $module['id'], 'type' => \HeimrichHannot\ReaderBundle\Backend\Module::MODULE_READER])) {
-                $this->output->writeln('Updated "' . $module['name'] . '" (Module ID:' . $module['id'] . ') and set new reader config ID: ' . $readerConfigId . '.');
+            $this->copyNewsTemplate();
+
+            $this->attachFilter();
+        }
+
+        return 0;
+    }
+
+    protected function attachFilter()
+    {
+        $this->filterConfig = [
+            'tstamp'        => time(),
+            'dateAdded'     => time(),
+            'title'         => $this->module['name'],
+            'name'          => \Contao\StringUtil::standardize($this->module['name']),
+            'dataContainer' => 'tl_news',
+            'method'        => 'GET',
+            'template'      => 'form_div_layout',
+            'published'     => 1
+        ];
+
+        $sql = "INSERT INTO tl_filter_config (" . implode(',', array_keys($this->filterConfig)) . ") VALUES (" . implode(',', array_map(function ($value) {
+                return ':' . $value;
+            }, array_keys($this->filterConfig))) . ")";
+
+        if ($this->queryBuilder->getConnection()->executeUpdate($sql, $this->readerConfig)) {
+            $this->filterConfig['id'] = $this->queryBuilder->getConnection()->lastInsertId();
+            $this->output->writeln('Created filter config for module "' . $this->module['name'] . '" (Module ID:' . $this->module['id'] . ') with ID: ' . $this->filterConfig['id'] . '.');
+
+
+            $updateSql = "UPDATE tl_reader_config SET filter = :filter WHERE id = :id";
+
+            if ($this->queryBuilder->getConnection()->executeUpdate($updateSql, ['filter' => $this->filterConfig['id'], 'id' => $this->readerConfig['id']])) {
+                $this->output->writeln('Updated reader config for "' . $this->module['name'] . '" (Module ID:' . $this->module['id'] . ') and set new filter config ID: ' . $this->filterConfig['id'] . '.');
+
+                $this->attachFilterElements();
+
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    protected function attachFilterElements()
+    {
+        $sorting = 2;
+        $pids    = \Contao\StringUtil::deserialize($this->module['news_archives'], true);
+
+        if (!empty($pids)) {
+            $initialValueArray = [];
+            foreach ($pids as $pid) {
+                $initialValueArray[] = ['value' => $pid];
             }
 
-            $templatePath     = \Controller::getTemplate($module['news_template']);
-            $twigTemplatePath = $this->getContainer()->get('huh.utils.container')->getProjectDir() . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $module['news_template'] . '.html.twig';
+            $filterElement                    = new FilterConfigElementModel();
+            $filterElement->pid               = $this->filterConfig['id'];
+            $filterElement->sorting           = $sorting;
+            $filterElement->tstamp            = time();
+            $filterElement->dateAdded         = time();
+            $filterElement->type              = 'parent';
+            $filterElement->isInitial         = 1;
+            $filterElement->field             = 'pid';
+            $filterElement->initialValueType  = 'array';
+            $filterElement->initialValueArray = serialize($initialValueArray);
+            $filterElement->published         = 1;
+            $filterElement->save();
 
-            if (!file_exists($twigTemplatePath)) {
-                $fileSystem = new Filesystem();
+            $sorting = $sorting * 2;
+        }
+    }
 
-                try {
-                    $fileSystem->copy($templatePath, $twigTemplatePath);
-                    $this->output->writeln('Created copy of existing template to ' . $module['news_template'] . '.html.twig template, please adjust the template to fit twig syntax in ' . $twigTemplatePath . '.');
-                } catch (FileNotFoundException $e) {
-                    $this->output->writeln('Could not copy news_template: ' . $module['news_template'] . ', which file does not exist.');
-                    return 1;
-                } catch (IOException $e) {
-                    $this->output->writeln('An error occurred while copy news_template from ' . $templatePath . ' to ' . $twigTemplatePath . '.');
-                    return 1;
-                }
+    protected function copyNewsTemplate()
+    {
+        $templatePath     = \Controller::getTemplate($this->module['news_template']);
+        $twigTemplatePath = $this->getContainer()->get('huh.utils.container')->getProjectDir() . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $this->module['news_template'] . '.html.twig';
+
+        if (!file_exists($twigTemplatePath)) {
+            $fileSystem = new Filesystem();
+
+            try {
+                $fileSystem->copy($templatePath, $twigTemplatePath);
+                $this->output->writeln('Created copy of existing template to ' . $this->module['news_template'] . '.html.twig template, please adjust the template to fit twig syntax in ' . $twigTemplatePath . '.');
+            } catch (FileNotFoundException $e) {
+                $this->output->writeln('Could not copy news_template: ' . $this->module['news_template'] . ', which file does not exist.');
+                return 1;
+            } catch (IOException $e) {
+                $this->output->writeln('An error occurred while copy news_template from ' . $templatePath . ' to ' . $twigTemplatePath . '.');
+                return 1;
             }
         }
 
         return 0;
+    }
+
+    protected function updateModule()
+    {
+        $updateSql = "UPDATE tl_module SET readerConfig = :readerConfig, `type`=:type WHERE id = :id";
+
+        if ($this->queryBuilder->getConnection()->executeUpdate($updateSql, ['readerConfig' => $this->readerConfig['id'], 'id' => $this->module['id'], 'type' => \HeimrichHannot\ReaderBundle\Backend\Module::MODULE_READER])) {
+            $this->output->writeln('Updated "' . $this->module['name'] . '" (Module ID:' . $this->module['id'] . ') and set new reader config ID: ' . $this->readerConfig['id'] . '.');
+
+            return 0;
+        }
+
+        return 1;
     }
 }
